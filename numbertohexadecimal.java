eee29aef0512d12428b719dafd4d532ab78a62d2
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2025-03-26 18:21:19 +0530

    Add files via upload

---
+++ numbertohexadecimal.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int num = 26;
+        String hex = Integer.toHexString(num); 
+        System.out.println(hex); 
+    }
+}
